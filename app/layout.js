afb26ceacf3b47951d28daf520555ab4b1dc350e
Vaughn Harrison <vaughn.isa@gmail.com>
2026-05-10 23:21:23 -0600

    Add Mexican Midwinters favicon

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,11 @@
 export const metadata = {
   title: 'ILCA Mexican Midwinter Regatta 2027',
   description: 'International Sailing Academy - March 11-13, 2027',
+  icons: {
+    icon: '/favicon.svg',
+    shortcut: '/favicon.svg',
+    apple: '/favicon.svg',
+  },
 }
 
 export default function RootLayout({ children }) {
